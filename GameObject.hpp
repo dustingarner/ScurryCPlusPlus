4b7384fee373d7674c8ad7f81e7042cabf3de5a1
Dustin Garner <144393035+dustingarner@users.noreply.github.com>
2024-01-08 23:50:50 -0800

    Add files via upload

--- GameObject.hpp
+++ GameObject.hpp
@@ -9,12 +9,14 @@
 #include "Input.hpp"
 #include "World.hpp"
 #include "Sprite.hpp"
+#include "Collision.hpp"
 
 using std::vector;
 
 
 class GameObject{
     public:
+    GameObject(sf::Vector2f _position) : position(_position) {}
     GameObject(sf::Vector2f _position, Sprite _sprite) : position(_position), sprite(_sprite) {}
     virtual ~GameObject();
     virtual void update(World& world, Input& input, double delta) = 0;
@@ -31,12 +33,17 @@ class GameObject{
 
 class MouseObject : GameObject{
     public:
-    MouseObject(sf::Vector2f _position, Sprite _sprite) : GameObject(_position, _sprite) {}
+    MouseObject(sf::Vector2f _position) : GameObject(_position) {}
     virtual void update(World& world, Input& input, double delta);
     virtual sf::Sprite getCurrentSprite();
+    void setControllable(bool toggle) {controllable = toggle;}
 
     private:
+    bool controllable = false;
     const int SPEED = 300.0;
+    CollisionBox enemyCollision = CollisionBox(sf::Vector2f(50.0, 25.0), sf::Vector2f(0.0,0.0));
+    CollisionBox shelterCollision = CollisionBox(sf::Vector2f(75.0, 36.0), sf::Vector2f(-22.3, -9.85));
+    CollisionBox cursorCollision = CollisionBox(sf::Vector2f(131.25, 75.0), sf::Vector2f(-70.0, -28.75));
 };
 
 MouseObject makeMouse();
